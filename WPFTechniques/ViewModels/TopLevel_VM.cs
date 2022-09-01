ef509b32ac1fe459ad4d25155e565ca69053fb6c
Patrick Luhmann <patrick.luhmann@gmail.com>
2022-09-01 12:28:11 -0700

    Create initial console and WPF projects
    Signed-off-by: Patrick Luhmann <patrick.luhmann@gmail.com>

---
+++ WPFTechniques/ViewModels/TopLevel_VM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFTechniques.ViewModels
+{
+	internal class TopLevel_VM
+	{
+		public string Salutation { get; set; } = "Welcome to TopLevel_VM";
+	}
+}
