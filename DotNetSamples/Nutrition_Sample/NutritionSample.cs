4cd71babc766f76a3c9109e561f479a1587607d6
Patrick Luhmann <patrick.luhmann@gmail.com>
2025-08-12 16:08:04 -0700

    Upgrade to .NET 8

--- DotNetSamples/Nutrition_Sample/NutritionSample.cs
+++ DotNetSamples/Nutrition_Sample/NutritionSample.cs
@@ -1,10 +1,4 @@
 ﻿using Nutrition.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DotNetSamples.Nutrition_Sample
 {
