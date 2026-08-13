5463a26db6b71b69a8589d23632df5391ee86dda
Patrick Luhmann <patrick.luhmann@gmail.com>
2026-08-13 15:19:32 -0700

    Add book create, index pages to blazor_cc

--- BookKlubKorner/BookKlubKorner.Domain/IBookKlubKornerService.cs
+++ BookKlubKorner/BookKlubKorner.Domain/IBookKlubKornerService.cs
@@ -6,11 +6,12 @@ public interface IBookKlubKornerService
 {
 	// Sync
 	List<Book> GetAllBooks();
-	void AddBook(string title, string author, string publisher, int pages, int year);
 
 	// Async
+	Task<int> GetCountOfBooksAsync();
 	Task<List<Book>> GetAllBooksAsync();
 	Task<Book?> GetBookByIdAsync(int id);
 	Task UpdateBookAsync(Book source);
 	Task DeleteBookAsync(int id);
+	Task<Book> CreateBookAsync(Book source);
 }
